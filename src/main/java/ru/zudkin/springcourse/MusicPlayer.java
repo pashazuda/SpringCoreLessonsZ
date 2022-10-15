d70b2ffac5379702678a4d982f30bdf89396bda2
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-15 08:11:32 +0300

    6. DI - setter

--- src/main/java/ru/zudkin/springcourse/MusicPlayer.java
+++ src/main/java/ru/zudkin/springcourse/MusicPlayer.java
@@ -3,10 +3,17 @@ package ru.zudkin.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer() {}
+
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
     }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
     }
