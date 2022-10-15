6e0c67e04f57232b2eaeba8991e220c044518697
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-15 10:12:23 +0300

    10. @Component

--- src/main/java/ru/zudkin/springcourse/ClassicalMusic.java
+++ src/main/java/ru/zudkin/springcourse/ClassicalMusic.java
@@ -1,21 +1,9 @@
 package ru.zudkin.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component("musicBean1")
 public class ClassicalMusic implements Music{
-
-    private ClassicalMusic() {}
-
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
-
-    public void doMyInit() {
-        System.out.println("Doing my initialization...");
-    }
-
-    public void doMyDestroy() {
-        System.out.println("Doing my destroy...");
-    }
-
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/ru/zudkin/springcourse/TestSpring.java
+++ src/main/java/ru/zudkin/springcourse/TestSpring.java
@@ -9,15 +9,9 @@ public class TestSpring {
         );
 
         ClassicalMusic musicBean1 = context.getBean("musicBean1", ClassicalMusic.class);
+        RockMusic musicBean2 = context.getBean("musicBean2", RockMusic.class);
         System.out.println(musicBean1.getSong());
-//        Music music = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-//
-//        musicPlayer.playMusic();
-////        System.out.println(musicPlayer.getName());
-////        System.out.println(musicPlayer.getVolume());
-////          System.out.println(musicPlayer.get)
+        System.out.println(musicBean2.getSong());
         context.close();
     }
 }
