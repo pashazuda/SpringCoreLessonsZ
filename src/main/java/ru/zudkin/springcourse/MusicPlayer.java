d53219c04c0486ab30dd28da33ba54be09914b12
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-15 08:19:04 +0300

    6. DI - внедрение значений

--- src/main/java/ru/zudkin/springcourse/MusicPlayer.java
+++ src/main/java/ru/zudkin/springcourse/MusicPlayer.java
@@ -2,6 +2,28 @@ package ru.zudkin.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private  int volume;
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer() {}
 
