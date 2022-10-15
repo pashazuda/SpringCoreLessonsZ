7ba7ed8da3c5aae90372748fda35f64edfa9d00b
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-15 08:45:19 +0300

    6. DI - дз: добавление списка

--- src/main/java/ru/zudkin/springcourse/MusicPlayer.java
+++ src/main/java/ru/zudkin/springcourse/MusicPlayer.java
@@ -1,13 +1,16 @@
 package ru.zudkin.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList();
     private String name;
     private  int volume;
 
-    public Music getMusic() {
-        return music;
-    }
+//    public Music getMusic() {
+//        return music;
+//    }
 
     public String getName() {
         return name;
@@ -28,15 +31,27 @@ public class MusicPlayer {
     public MusicPlayer() {}
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
---
+++ src/main/java/ru/zudkin/springcourse/RapMusic.java
@@ -0,0 +1,8 @@
+package ru.zudkin.springcourse;
+
+public class RapMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Wolves";
+    }
+}
