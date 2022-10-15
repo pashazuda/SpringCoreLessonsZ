a4ab42f247887344df6134532036198d81cb866e
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-15 10:40:20 +0300

    10. @Autowired для 2ух бинов в 1 плеере

--- src/main/java/ru/zudkin/springcourse/MusicPlayer.java
+++ src/main/java/ru/zudkin/springcourse/MusicPlayer.java
@@ -7,18 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class MusicPlayer {
+
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
     @Autowired
-    private Music music;
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + classicalMusic.getSong());
+        System.out.println("Playing: " + rockMusic.getSong());
     }
 }
--- src/main/java/ru/zudkin/springcourse/RockMusic.java
+++ src/main/java/ru/zudkin/springcourse/RockMusic.java
@@ -2,7 +2,7 @@ package ru.zudkin.springcourse;
 
 import org.springframework.stereotype.Component;
 
-//@Component("musicBean2")
+@Component("musicBean2")
 public class RockMusic implements Music{
     @Override
     public String getSong() {
